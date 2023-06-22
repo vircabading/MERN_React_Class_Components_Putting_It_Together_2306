7e4a8a5639caea187d9f1a23749e74786dd39030
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-21 23:36:00 -0500

    add state to person card

--- src/components/PersonCardComp.jsx
+++ src/components/PersonCardComp.jsx
@@ -4,14 +4,27 @@ import React, {Component} from "react";
 // PersonCardComp.jsx
 
 class PersonCardComp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: this.props.firstName,
+            lastName: this.props.lastName,
+            age: this.props.age,
+            hairColor: this.props.hairColor
+        }
+    }
+
     render() {
         // Props passed from parent component
-        const {firstName, lastName, age, hairColor} = this.props;
+        //const {firstName, lastName, age, hairColor} = this.props;
 
         return <div>
-            <h1>{ lastName }, { firstName }</h1>
-            <p>Age: { age }</p>
-            <p>Hair Color: { hairColor }</p>
+            <h1>{ this.state.lastName }, { this.state.firstName }</h1>
+            <p>Age: { this.state.age }</p>
+            <p>Hair Color: { this.state.hairColor }</p>
+            <br />
+            <button>Birthday Button for { this.state.firstName } { this.state.lastName }</button>
             <br></br>
         </div>;
     }
