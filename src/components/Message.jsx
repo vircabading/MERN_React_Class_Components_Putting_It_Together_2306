09aff9cd4ccad21ccbfd49d184a9c2f1136b1251
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-21 22:54:10 -0500

    create message component

---
+++ src/components/Message.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Message extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Message Here</h1>
+                <button>Leave</button>
+            </div>
+        );
+
+    }
+}
+
+export default Message;
