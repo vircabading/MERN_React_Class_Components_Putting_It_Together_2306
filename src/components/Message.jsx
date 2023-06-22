418f12b4eb975368dfa2e44c615e5ee72302930b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-21 23:03:06 -0500

    create constructor in Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 
 class Message extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
